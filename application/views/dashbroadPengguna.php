f341c8529e009dbc03c7f2a8a23d36b34ca4c754
Dickygg <dickyyfebrian43@gmail.com>
2025-07-05 20:40:13 +0700

    final home page

--- application/views/dashbroadPengguna.php
+++ application/views/dashbroadPengguna.php
@@ -34,17 +34,23 @@
 
         }
 
-        @media screen and (max-width: 1440px) {}
+        @media screen and (max-width: 1440px) {
+            #wave {
+                /* display: none; */
+                margin-top: -120px;
+            }
 
-        @media screen and (min-width: 1440px) {
-            #home-left img {}
+
+
         }
+
+        @media screen and (min-width: 1440px) {}
     </style>
 </head>
 
 <body id="page-top" style="background-color: BDC3C7;">
     <!--navbar  -->
-    <div id="wrapper">
+    <div id="wrapper" class="sticky-top">
         <!-- Navbar -->
         <nav class="navbar navbar-expand-lg border-bottom" style="background-color: rgb(243, 247, 247);">
             <div class="container">
@@ -110,13 +116,141 @@
         <path fill="#fff" fill-opacity="1" d="M0,160L60,176C120,192,240,224,360,213.3C480,203,600,149,720,149.3C840,149,960,203,1080,213.3C1200,224,1320,192,1380,176L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
     </svg>
 
-    <section id="about">
+    <!-- <section id="about">
         <div class="container">
             <div class="row">
 
             </div>
         </div>
+    </section> -->
+    <section id="steps" class="py-5">
+        <div class="container text-center">
+            <h3 class="fw-bold mb-5">Langkah-Langkah Pengaduan</h3>
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-3 p-4 p-lg-0">
+                    <i class="bi bi-pencil-square display-4 text-primary"></i>
+                    <h5 class="mt-3">Buat Pengaduan</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem temporibus pariatur totam labore dignissimos, ipsam eos,</p>
+                </div>
+                <div class="col-md-3 p-4 p-lg-0">
+                    <i class="bi bi-headset display-4 text-primary"></i>
+                    <h5 class="mt-3">Verifikasi</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem temporibus pariatur totam labore dignissimos, ipsam eos,</p>
+
+                </div>
+                <div class="col-md-3 p-4 p-lg-0">
+                    <i class="bi bi-gear display-4 text-primary"></i>
+                    <h5 class="mt-3">Diproses</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem temporibus pariatur totam labore dignissimos, ipsam eos,</p>
+
+                </div>
+                <div class="col-md-3 p-4 p-lg-0">
+                    <i class="bi bi-check-circle display-4 text-primary"></i>
+                    <h5 class="mt-3">Selesai</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem temporibus pariatur totam labore dignissimos, ipsam eos,</p>
+
+                </div>
+            </div>
+        </div>
     </section>
+    <!-- footer -->
+    <!-- Footer -->
+    <footer class="text-center text-lg-start bg-body-tertiary text-muted">
+        <!-- Section: Social media -->
+        <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+            <!-- Left -->
+            <div class="me-5 d-none d-lg-block">
+                <span>Get connected with us on social networks:</span>
+            </div>
+            <!-- Left -->
+
+            <!-- Right -->
+            <div>
+                <a href="" class="me-4 text-reset">
+                    <i class="bi bi-facebook"></i>
+                </a>
+                <a href="" class="me-4 text-reset">
+                    <i class="bi bi-twitter"></i>
+                </a>
+                <a href="" class="me-4 text-reset">
+                    <i class="bi bi-google"></i>
+                </a>
+                <a href="" class="me-4 text-reset">
+                    <i class="bi bi-instagram"></i>
+                </a>
+            </div>
+            <!-- Right -->
+        </section>
+        <!-- Section: Social media -->
+
+        <!-- Section: Links  -->
+        <section class="">
+            <div class="container text-center text-md-start mt-5">
+                <!-- Grid row -->
+                <div class="row mt-3">
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                        <!-- Content -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            <i class="fas fa-gem me-3"></i>Company name
+                        </h6>
+                        <p>
+                            Here you can use rows and columns to organize your footer content. Lorem ipsum
+                            dolor sit amet, consectetur adipisicing elit.
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Useful links
+                        </h6>
+                        <p>
+                            <a href="#!" class="text-reset">Pricing</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Settings</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Orders</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Help</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+                        <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+                        <p>
+                            <i class="fas fa-envelope me-3"></i>
+                            info@example.com
+                        </p>
+                        <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+                        <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+                    </div>
+                    <!-- Grid column -->
+                </div>
+                <!-- Grid row -->
+            </div>
+        </section>
+        <!-- Section: Links  -->
+
+        <!-- Copyright -->
+        <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+            © 2021 Copyright:
+            <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
 
 
 
